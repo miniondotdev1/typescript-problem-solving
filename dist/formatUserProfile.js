74f4928cf06aae51464bb4b4b2deb3cf5e7b6b8d
my first github repo <mrarifmondol98@gmail.com>
2026-08-20 10:27:59 +0600

    typescript problem solving

--- dist/formatUserProfile.js
+++ dist/formatUserProfile.js
@@ -1,4 +1,9 @@
 "use strict";
+// type User = {
+//     name: string
+//     age: number
+//     city: string
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
 const formatUserProfile = (user) => {
     return `${user.name} is ${user.age} years old and lives in ${user.city}.`;
--- src/formatUserProfile.ts
+++ src/formatUserProfile.ts
@@ -1,10 +1,15 @@
 
-type User = {
+// type User = {
+//     name: string
+//     age: number
+//     city: string
+// }
+
+interface User{
     name: string
     age: number
     city: string
 }
-
 
 
 const formatUserProfile = (user: User): string => {
